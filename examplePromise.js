c450a7cebd9c0e4a40ecbf6e87095be3c58ca1cc
hectorcoronado <coronado.hector@gmail.com>
2016-09-07 20:03:03 -0700

    examplePromise.js, simple examples of using promises in JS

---
+++ examplePromise.js
@@ -0,0 +1,46 @@
+/*
+This is just a couple of simple examples of using promises in JS, not strictly speaking a coding challenge or algorithm.
+
+setTimeout is used only to emulate the span of time it may take to resolve or reject a promise.
+*/
+
+function getTempPromise (location) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve(80);
+      reject('City not found');
+    }, 1000);
+  });
+}
+
+getTempPromise('Oakland').then(function (temp) {
+  console.log('The temperature in Oakland is: ', temp, ' degrees.');
+}, function(err) {
+  console.log('Promise error: ', err);
+});
+
+Challenge Area:
+
+function addPromise (a, b) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function() {
+      if (typeof a === 'number' && typeof b === 'number') {
+        resolve(a + b);
+      } else {
+        reject('Need to pass in numbers');
+      }
+    }, 2000);
+  });
+}
+
+addPromise(2, 2).then(function(sum) {
+  console.log('Promise success: ', sum);
+}, function(err) {
+  console.log('Promise error: ', err);
+});
+
+addPromise(3, 'no').then(function(sum) {
+  console.log('Promise success: ', sum);
+}, function(err) {
+  console.log('Promise error: ', err);
+});
