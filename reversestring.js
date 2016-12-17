00da63588345d8f6a92063f70e3a3f075aa7b0c3
hectorcoronado <coronado.hector@gmail.com>
2016-12-17 11:39:11 -0800

    added another solution to reverseString

--- reversestring.js
+++ reversestring.js
@@ -6,3 +6,15 @@ function reverseString(str) {
   str = str.split('').reverse().join('');
   return str;
 }
+
+/*
+Another (faster?) solution
+*/
+
+var reverseString = function(s) {
+  let revString = '';
+  for (var i = s.length - 1; i >= 0; i--) {
+    revString += s[i];
+  }
+  return revString;
+};
