e223aaa1dc42beeceb424fb5047cf9e5cfbb57e8
hectorcoronado <coronado.hector@gmail.com>
2017-01-05 21:24:40 -0800

    eloquent js ch 4 range.js & sum.js

---
+++ eloquent-javascript-exercises/chapter4/range.js
@@ -0,0 +1,16 @@
+/*
+Write a range function that takes two arguments,
+start and end,
+and returns an array containing all the numbers
+from start up to (and including) end.
+*/
+
+function range(start, end) {
+  var result = []
+  for (var i = start; i <= end; i++) {
+    result.push(i);
+  }
+  return result;
+}
+
+range(1, 10);
