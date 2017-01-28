8ed52b0231cca2d9f8b093fe0e41c709bcf29db5
hectorcoronado <coronado.hector@gmail.com>
2017-01-27 18:17:25 -0800

    EJS ch5 reduce.js added

--- eloquent-javascript-exercises/chapter5/reduce.js
+++ eloquent-javascript-exercises/chapter5/reduce.js
@@ -1,3 +1,36 @@
 /*
-A common pattern 
+A common pattern of computation on arrays is computing a single value from them.
+
+The higher-order operation that represents this pattern is called 'reduce' -- it can be thought of as folding up the array, one element at a time.
+
+The parameters to the 'reduce' function are, apart from the array, a combining function and a start value.
 */
+
+function reduce(array, combine, start) {
+  var current = start;
+
+  for (var i = 0; i < array.length; i++) {
+    current = combine(current, array[i]);
+  }
+
+  return current;
+}
+
+/*
+If the array contains at least one element, 'start' argument may be may be left empty. The method will take the first element of the array as its start value and start reducing at the second element.
+
+The following implementation of reduce() finds the most ancient known ancestor:
+*/
+
+var ANCESTRY_FILE = require('./ancestryFile');
+
+var ancestry = JSON.parse(ANCESTRY_FILE);
+
+
+console.log(ancestry.reduce(function(min, cur) {
+  if (cur.born < min.born) {
+    return cur;
+  } else {
+    return min;
+  }
+}));
