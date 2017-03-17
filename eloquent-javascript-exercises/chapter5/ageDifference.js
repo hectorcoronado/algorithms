1affc2a79bb06680ae963df3c9855dbfee18f328
hectorcoronado <coronado.hector@gmail.com>
2017-03-17 16:59:04 -0700

    ageDifference ch5 soluion

---
+++ eloquent-javascript-exercises/chapter5/ageDifference.js
@@ -0,0 +1,35 @@
+/*
+Compute the average age difference between mothers and children (the age of the mother when the child is born).
+*/
+
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
+// Your code here.
+var ageDifferences = [];
+
+function ageDifference() {
+
+  for(var i = 0; i < ancestry.length; i++) {
+    var child = ancestry[i];
+
+    if(child.mother !== null && byName[child.mother] !== undefined) {
+      var mother = byName[child.mother];
+      var ageDifference = child.born - mother.born;
+
+      ageDifferences.push(ageDifference);
+    }
+  }
+  return ageDifferences;
+}
+
+ageDifference();
+console.log(average(ageDifferences));
+// → 31.2
