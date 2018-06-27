440a6009306ff50a266a22cc6db22a136cfab77e
hectorcoronado <hectorcoronado.developer@gmail.com>
2018-06-26 21:15:26 -0700

    solves for chunk

--- algoCasts/algorithms2/exercises/chunk/index.js
+++ algoCasts/algorithms2/exercises/chunk/index.js
@@ -8,6 +8,63 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// const chunk = (array, size) => {
+//   let result = []
+//
+//   if (array.length < size) {
+//     return result.push(array)
+//   }
+//
+//   while (array.length) {
+//     result.push(array.splice(0, size))
+//   }
+//   return result
+// }
 
-module.exports = chunk;
+/**
+  * 1. create empty array to hold `chunks`
+  * 2. for each element in the `unchunked` array:
+  *   - retrieve the last element in the `chunked` array
+  *   - if last element does not exist, or its length is equal
+  *     to chunk size
+  *       - push a **new** chunk into `chunked` with the current element
+  *   - *else* add the current element into the chunk
+  */
+
+// const chunk = (array, size) => {
+//   let chunked = []
+//
+//   for (let element of array) {
+//     let last = chunked[chunked.length - 1]
+//
+//     if (!last || last.length === size) {
+//       chunked.push([element])
+//     } else {
+//       last.push(element)
+//     }
+//   }
+//
+//   return chunked
+// }
+
+/**
+  * 1.  create empty `chunked` array
+  * 2.  create `index` var starting at 0
+  * 3.  while index is less than array.length
+  *       - push a slice of length `size` from `array` into `chunked`
+  *       - add `size` to `index`
+  */
+
+const chunk = (array, size) => {
+  let chunked = []
+  let index = 0
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size))
+    index += size
+  }
+
+  return chunked
+}
+
+module.exports = chunk
