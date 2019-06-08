69040ccedd8a6526dba75432048c9f311639db30
hectorcoronado <hectorcoronado.developer@gmail.com>
2019-06-08 12:35:13 -0700

    refactors multiplePointers/areThereDuplicateArgs.js

--- jsAlgosAndDataStructuresMasterClass/multiplePointers/areThereDuplicateArgs.js
+++ jsAlgosAndDataStructuresMasterClass/multiplePointers/areThereDuplicateArgs.js
@@ -10,26 +10,51 @@
  * areThereDuplicateArgs('a', 'b', 'c', 'a') // true
  */
 
+// const areThereDuplicateArgs = (...args) => {
+//     if (!args) return false
+
+//     let left = 0
+//     let right = args.length - 1
+
+//     while (left < right || left === right) {
+//         if (left !== right && args[left] === args[right]) {
+//             return true
+//         } else if (right > left) {
+//             right -= 1
+//         } else {
+//             left += 1
+//             right = args.length - 1
+//         }
+//     }
+
+//     return false
+// }
+
+/**
+ * a better (more readable, less complex) implementation of the above
+ */
+// const areThereDuplicates = (...args) => {
+//     // Two pointers
+//     args.sort((a, b) => a > b);
+//     let start = 0;
+//     let next = 1;
+//     while (next < args.length) {
+//         if (args[start] === args[next]) {
+//             return true
+//         }
+//         start++
+//         next++
+//     }
+//     return false
+// }
+
+/**
+ * one liner using `Set`
+ */
+
 const areThereDuplicateArgs = (...args) => {
-    if (!args) return false
-
-    let left = 0
-    let right = args.length - 1
-
-    while (left < right || left === right) {
-        if (left !== right && args[left] === args[right]) {
-            return true
-        } else if (right > left) {
-            right -= 1
-        } else {
-            left += 1
-            right = args.length - 1
-        }
-    }
-
-    return false
+    return new Set(args).size !== args.length;
 }
-
 console.log(areThereDuplicateArgs(1, 2, 3)) // false
 console.log(areThereDuplicateArgs(1, 2, 2)) // true
 console.log(areThereDuplicateArgs('a', 'b', 'c', 'a')) // true
